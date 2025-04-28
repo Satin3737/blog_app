2e5e76c22d0396b91ea4a8b601d531ad9534aadf
satin <satin3737@gmail.com>
2025-04-28 21:13:22 +0300

    Enhance showSnackBar function to support multiple Snackbar types for improved user feedback

--- lib/core/utils/show_snackbar.dart
+++ lib/core/utils/show_snackbar.dart
@@ -1,14 +1,24 @@
 import 'package:blog_app/core/theme/app_pallet.dart';
 import 'package:flutter/material.dart';
 
-void showSnackBar(BuildContext context, String message) {
+enum SnackBarType { success, info, error }
+
+void showSnackBar(
+  BuildContext context,
+  String message, [
+  SnackBarType type = SnackBarType.error,
+]) {
   ScaffoldMessenger.of(context)
     ..hideCurrentSnackBar()
     ..showSnackBar(
       SnackBar(
         content: Text(message, style: const TextStyle(color: AppPallet.white)),
         behavior: SnackBarBehavior.floating,
-        backgroundColor: AppPallet.error,
+        backgroundColor: switch (type) {
+          SnackBarType.success => AppPallet.gradient1,
+          SnackBarType.info => AppPallet.gradient2,
+          SnackBarType.error => AppPallet.error,
+        },
         margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
